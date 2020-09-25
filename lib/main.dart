6ea9284406bf7e9d651da4cff2fbfe22305717ba
Hoony <kimyh03@gmail.com>
2020-09-25 20:08:17 +0900

    Create character card

--- lib/main.dart
+++ lib/main.dart
@@ -6,27 +6,113 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "First app",
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyCard(),
+      title: "Monster",
+      home: Grade(),
     );
   }
 }
 
-class MyCard extends StatelessWidget {
+class Grade extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.yellow[100],
       appBar: AppBar(
-        title: Text("Hello Monster!"),
+        title: Text("HOONY"),
+        backgroundColor: Colors.yellow[300],
         centerTitle: true,
-        backgroundColor: Colors.greenAccent,
-        elevation: 0,
+        elevation: 0.0,
       ),
-      body: Center(
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text("Hello"), Text("Hello"), Text("Hello")],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage("assets/짱구.png"),
+                backgroundColor: Colors.yellow[300],
+                radius: 60.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.white,
+              thickness: 0.5,
+            ),
+            Text(
+              "NAME",
+              style: TextStyle(color: Colors.black, letterSpacing: 2.0),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "Hoony",
+              style: TextStyle(
+                  color: Colors.black,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              "HOONY POWER LEVEL",
+              style: TextStyle(color: Colors.black, letterSpacing: 2.0),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "77",
+              style: TextStyle(
+                  color: Colors.black,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  "Super attack!",
+                  style: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  "Super attack!",
+                  style: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  "Super attack!",
+                  style: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
